e3d583212033e843073b1187c32e57447b6eca65
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2023-04-15 22:15:27 +0200

    Circular reflective boundary condition.

--- src/abp_2D_confine.cpp
+++ src/abp_2D_confine.cpp
@@ -118,6 +118,24 @@ void check_nooverlap(
 	}
 }
 
+void circular_reflective_boundary_conditions(
+	double *x, double *y, int Particles,
+	double Wall, int L
+)
+{
+	double distance_squared = 0.0, Wall_squared = Wall * Wall;
+#pragma omp parallel for simd num_threads(N_thread)	
+	for (int k = 0; k < Particles; k++)
+	{
+		distance_squared = x[k]*x[k] + y[k]*y[k];
+		if (distance_squared > Wall_squared)
+		{
+			x[k] = (sqrt(Wall_squared) / sqrt(distance_squared)) * x[k];
+			y[k] = (sqrt(Wall_squared) / sqrt(distance_squared)) * y[k];
+		}
+	}
+}
+
 void reflective_boundary_conditions(
 	double *x, double *y, int Particles,
 	double Wall, int L
