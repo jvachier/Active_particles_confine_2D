33b306af855fb9e138f921eceb9cba06df53d43f
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2024-12-29 17:26:56 +0100

    All .h files.

--- src/headers/check_nooverlap.h
+++ src/headers/check_nooverlap.h
@@ -1,12 +1,15 @@
+#include <omp.h>
+#include <time.h>
+#include <stdio.h>
 #include <iostream>
 #include <random>
 #include <cstring>
-#include <stdio.h>
 #include <cmath>
-#include <time.h>
-#include <omp.h> //import library to use pragma
+
+
 
 void check_nooverlap(
-	double *x, double *y, int Particles,
-	int L,
-	std::default_random_engine &generator, std::uniform_real_distribution<double> &distribution);
+  double *x, double *y, int Particles,
+  int L,
+  std::default_random_engine &generator, 
+  std::uniform_real_distribution<double> &distribution);
